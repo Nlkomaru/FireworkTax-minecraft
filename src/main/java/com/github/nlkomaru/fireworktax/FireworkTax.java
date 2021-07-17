80c88fbbad66ce2d20d96c2ab4bff29fe33d3864
Nlkomaru <76208219+Nlkomaru@users.noreply.github.com>
2021-07-18 03:18:55 +0900

    Initial commit

---
+++ src/main/java/com/github/nlkomaru/fireworktax/FireworkTax.java
@@ -0,0 +1,48 @@
+package com.github.nlkomaru.fireworktax;
+
+
+import com.github.nlkomaru.fireworktax.tax.BoostCheck;
+import org.bukkit.plugin.java.JavaPlugin;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+
+
+public final class FireworkTax extends JavaPlugin {
+
+    private static Economy econ = null;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+
+        if (!setupEconomy()) {
+            System.out.println("No economy plugin found. Disabling Vault");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+        getServer().getPluginManager().registerEvents(new BoostCheck(),this);
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+
+    }
+
+
+    public static Economy getEconomy()
+    {
+
+        return econ;
+    }
+
+}
---
+++ src/main/java/com/github/nlkomaru/fireworktax/tax/BoostCheck.java
@@ -0,0 +1,41 @@
+package com.github.nlkomaru.fireworktax.tax;
+
+
+import com.github.nlkomaru.fireworktax.FireworkTax;
+
+import com.github.nlkomaru.fireworktax.files.config;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.economy.EconomyResponse;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+
+public class BoostCheck implements Listener {
+
+
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent e) {
+        Economy eco = FireworkTax.getEconomy();
+        Player p = e.getPlayer();
+        int f = config.getFirework();
+        if (p.isGliding()) {
+            if (p.getInventory().getItemInMainHand().getType().equals(Material.FIREWORK_ROCKET) || p.getInventory().getItemInOffHand().getType().equals(Material.FIREWORK_ROCKET)) {
+                if (e.getAction().equals(Action.RIGHT_CLICK_AIR) || e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+                    if (eco.getBalance(p) <= f-1) {
+                        e.setCancelled(true);
+                        p.sendMessage(ChatColor.YELLOW + "お金がないのでブースト飛行できません");
+                    } else {
+                        EconomyResponse response = eco.withdrawPlayer(p, f);
+                    }
+                }
+            }
+        }
+    }
+}
+
