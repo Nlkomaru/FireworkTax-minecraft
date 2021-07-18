d0cd7336e498c0ceb36c5106235a96801dce6ff1
Nlkomaru <76208219+Nlkomaru@users.noreply.github.com>
2021-07-18 10:09:25 +0900

    金額が正常に反映されない件を修正

--- src/main/java/com/github/nlkomaru/fireworktax/tax/BoostCheck.java
+++ src/main/java/com/github/nlkomaru/fireworktax/tax/BoostCheck.java
@@ -23,15 +23,14 @@ public class BoostCheck implements Listener {
     public void onPlayerInteract(PlayerInteractEvent e) {
         Economy eco = FireworkTax.getEconomy();
         Player p = e.getPlayer();
-        int f = config.getFirework();
         if (p.isGliding()) {
             if (p.getInventory().getItemInMainHand().getType().equals(Material.FIREWORK_ROCKET) || p.getInventory().getItemInOffHand().getType().equals(Material.FIREWORK_ROCKET)) {
                 if (e.getAction().equals(Action.RIGHT_CLICK_AIR) || e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
-                    if (eco.getBalance(p) <= f-1) {
+                    if (eco.getBalance(p) <= config.getFirework()-1) {
                         e.setCancelled(true);
                         p.sendMessage(ChatColor.YELLOW + "お金がないのでブースト飛行できません");
                     } else {
-                        EconomyResponse response = eco.withdrawPlayer(p, f);
+                        EconomyResponse response = eco.withdrawPlayer(p, config.getFirework());
                     }
                 }
             }
