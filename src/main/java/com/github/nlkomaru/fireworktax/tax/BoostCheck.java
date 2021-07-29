909244c60ce1202d7c6a531712125ac87758c65d
Nlkomaru <76208219+Nlkomaru@users.noreply.github.com>
2021-07-30 03:43:13 +0900

    Change from Player to UUID-based management

--- src/main/java/com/github/nlkomaru/fireworktax/tax/BoostCheck.java
+++ src/main/java/com/github/nlkomaru/fireworktax/tax/BoostCheck.java
@@ -6,8 +6,10 @@ import com.github.nlkomaru.fireworktax.FireworkTax;
 import com.github.nlkomaru.fireworktax.files.Customconfig;
 import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.economy.EconomyResponse;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -16,25 +18,26 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.util.HashMap;
+import java.util.UUID;
 
 
 public class BoostCheck implements Listener {
 
-    HashMap<Player, Float> total = new HashMap<>();
-    HashMap<Player, Integer> d = new HashMap<>();
+    HashMap<UUID, Float> total = new HashMap<>();
+    HashMap<UUID, Integer> d = new HashMap<>();
 
-    @EventHandler
-    public void onJoin(PlayerJoinEvent e) {
-        d.put(e.getPlayer(), 0);
-        total.put(e.getPlayer(), (float) 0);
-    }
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent e) {
         Economy eco = FireworkTax.getEconomy();
         Player p = e.getPlayer();
+        UUID uuid = e.getPlayer().getUniqueId();
         float f = Customconfig.getFirework();
         int c = Customconfig.getCount();
+        if(total.get(uuid) == null){
+            d.put(e.getPlayer().getUniqueId(), 0);
+            total.put(e.getPlayer().getUniqueId(), (float) 0);
+        }
 
         if (p.isGliding()) {
             if (p.getInventory().getItemInMainHand().getType().equals(Material.FIREWORK_ROCKET) || p.getInventory().getItemInOffHand().getType().equals(Material.FIREWORK_ROCKET)) {
@@ -45,12 +48,12 @@ public class BoostCheck implements Listener {
                     } else {
                         EconomyResponse response = eco.withdrawPlayer(p, f);
                         if (c != 0) {
-                            total.put(p, total.get(p) + f);
-                            d.put(p, d.get(p) + 1);
-                            if (d.get(p) >= c) {
-                                p.sendMessage(ChatColor.YELLOW + String.format("%d回ブーストして合計で%.2f円引かれました", d.get(p), total.get(p)));
-                                d.put(p, 0);
-                                total.put(p, (float) 0);
+                            total.put(uuid, total.get(uuid) + f);
+                            d.put(uuid, d.get(uuid) + 1);
+                            if (d.get(uuid) >= c) {
+                                p.sendMessage(ChatColor.YELLOW + String.format("%d回ブーストして合計で%.2f円引かれました", d.get(uuid), total.get(uuid)));
+                                d.put(uuid, 0);
+                                total.put(uuid, (float) 0);
                             }
                         }
                     }
