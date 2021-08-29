81327d6964872e60d201e19e12c38d897c44ecb1
Nlkomaru <76208219+Nlkomaru@users.noreply.github.com>
2021-08-30 07:53:05 +0900

    To use PlayerElytraBoostEvent

--- src/main/java/dev/nikomaru/fireworktax/tax/BoostCheck.java
+++ src/main/java/dev/nikomaru/fireworktax/tax/BoostCheck.java
@@ -1,60 +1,47 @@
 package dev.nikomaru.fireworktax.tax;
 
-
+import com.destroystokyo.paper.event.player.PlayerElytraBoostEvent;
 import dev.nikomaru.fireworktax.FireworkTax;
-
 import dev.nikomaru.fireworktax.files.Customconfig;
+import java.util.HashMap;
+import java.util.UUID;
 import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.economy.EconomyResponse;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.player.PlayerInteractEvent;
-
-import java.util.HashMap;
-import java.util.UUID;
-
 
 public class BoostCheck implements Listener {
 
-    HashMap<UUID, Float> total = new HashMap<>();
-    HashMap<UUID, Integer> d = new HashMap<>();
+    HashMap<UUID, Float> total = new HashMap<> ();
+    HashMap<UUID, Integer> d = new HashMap<> ();
 
+    @EventHandler public void onPlayerBoost (PlayerElytraBoostEvent e) {
+        Economy eco = FireworkTax.getEconomy ();
+        Player p = e.getPlayer ();
+        UUID uuid = e.getPlayer ().getUniqueId ();
+        float f = Customconfig.getFirework ();
+        int c = Customconfig.getCount ();
 
-    @EventHandler
-    public void onPlayerInteract(PlayerInteractEvent e) {
-        Economy eco = FireworkTax.getEconomy();
-        Player p = e.getPlayer();
-        UUID uuid = e.getPlayer().getUniqueId();
-        float f = Customconfig.getFirework();
-        int c = Customconfig.getCount();
-
-        if(total.get(uuid) == null){
-            d.put(uuid, 0);
-            total.put(uuid, (float) 0);
+        if (total.get (uuid) == null) {
+            d.put (uuid, 0);
+            total.put (uuid, (float) 0);
         }
 
-        if (p.isGliding()) {
-            if (p.getInventory().getItemInMainHand().getType().equals(Material.FIREWORK_ROCKET) || p.getInventory().getItemInOffHand().getType().equals(Material.FIREWORK_ROCKET)) {
-                if (e.getAction().equals(Action.RIGHT_CLICK_AIR)) {
-                    if (eco.getBalance(p) < f) {
-                        e.setCancelled(true);
-                        p.sendMessage(ChatColor.YELLOW + "お金がないのでブースト飛行できません");
-                    } else {
-                        EconomyResponse response = eco.withdrawPlayer(p, f);
-                        if (c != 0) {
-                            total.put(uuid, total.get(uuid) + f);
-                            d.put(uuid, d.get(uuid) + 1);
-                            if (d.get(uuid) >= c) {
-                                p.sendMessage(ChatColor.YELLOW + String.format("%d回ブーストして合計で%.2f円引かれました", d.get(uuid), total.get(uuid)));
-                                d.put(uuid, 0);
-                                total.put(uuid, (float) 0);
-                            }
-                        }
-                    }
+        if (eco.getBalance (p) < f) {
+            e.setCancelled (true);
+            p.sendMessage (ChatColor.YELLOW + "お金がないのでブースト飛行できません");
+        } else {
+            EconomyResponse response = eco.withdrawPlayer (p, f);
+            if (c != 0) {
+                total.put (uuid, total.get (uuid) + f);
+                d.put (uuid, d.get (uuid) + 1);
+                if (d.get (uuid) >= c) {
+                    p.sendMessage (ChatColor.YELLOW + String.format ("%d回ブーストして合計で%.2f円引かれました", d.get (uuid),
+                            total.get (uuid)));
+                    d.put (uuid, 0);
+                    total.put (uuid, (float) 0);
                 }
             }
         }
